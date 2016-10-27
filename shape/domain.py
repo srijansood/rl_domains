b6a94a0dea89814befe5aa7db2c3f3a9e107431c
Srijan Sood <srijansood@gmail.com>
2016-10-27 04:22:34 -0400

    Basic domain completed with constant reward

--- shape/domain.py
+++ shape/domain.py
@@ -7,18 +7,19 @@ import numpy as np
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+ACTIONS = ["up", "down", "left", "right"]
+
 
 class params:
     grid_size = [10, 10]
     grid_to_pixel = 8
     img_size = [g * grid_to_pixel for g in grid_size] + [3]
     # shapes = "square" or "circle"
-    agent_shape = "square"
+    agent_shape = "circle"
     color = (255, 0, 0)
 
-class ShapeWorld:
 
-    ACTIONS = ["up", "down", "left", "right"]
+class ShapeWorld:
 
     def __init__(self, params):
         self.grid_size = params.grid_size
@@ -39,112 +40,97 @@ class ShapeWorld:
 
         # Initialize pygame
         pygame.init()
-        done = False
 
         # Set the HEIGHT and WIDTH of the screen (for pygame)
-        WINDOW_SIZE = [255, 255]
+        WINDOW_SIZE = self.screen_size[0:2]
         self.screen = pygame.display.set_mode(WINDOW_SIZE)
 
         # Set title of screen
         pygame.display.set_caption("Shape-World")
-
-        # Loop until the user clicks the close button.
-        self.done = False
+        self.screen.fill(BLACK)
 
         # Used to manage how fast the screen updates
         self.clock = pygame.time.Clock()
+        self.clock.tick(60) # 60fps
 
         # Initialize agent
         self.x = 0
         self.y = 0
         self.shape = params.agent_shape
         self.color = params.color
+        self.draw_agent()
+        self.steps = 0
 
-        while not done:
-            for event in pygame.event.get():  # User did something
-                if event.type == pygame.QUIT:  # If user clicked close
-                    done = True
-
-            # Set the screen background
-            self.screen.fill(BLACK)
-
-            # Draw the grid
-            for row in range(10):
-                for column in range(10):
-                    color = WHITE
-            self.draw_agent()
-
-            # Limit to 60 frames per second
-            self.clock.tick(60)
-
-            # Go ahead and update the screen with what we've drawn.
-            pygame.display.flip()
+        pygame.display.flip()
 
     def draw_agent(self):
         if self.shape == "circle":
-            pygame.draw.circle(self.screen, self.color, [(self.margin + self.width) * self.x + self.margin,
-                                               (self.margin + self.height) * self.y + self.margin], 10)
+            rad = self.ratio/2
+            pygame.draw.circle(self.screen, self.color, [(self.margin + self.width) * self.y + self.margin + rad,
+                                               (self.margin + self.height) * self.x + self.margin + rad], rad)
         else:
             pygame.draw.rect(self.screen,
                              self.color,
-                             [(self.margin + self.width) * self.x + self.margin,
-                              (self.margin + self.height) * self.y + self.margin,
+                             [(self.margin + self.width) * self.y + self.margin,
+                              (self.margin + self.height) * self.x + self.margin,
                               self.width,
                               self.height])
 
-    # def draw(self, screen_arr):
-    #     # Set the screen background
-    #     self.screen.fill(display.BLACK)
-    #
-    #     print(screen_arr)
-    #
-    #     # Draw the grid
-    #     for row in range(self.grid_size[0]):
-    #         for column in range(self.grid_size[1]):
-    #             color = display.WHITE
-    #
-    #             pygame.draw.rect(self.screen,
-    #                              color,
-    #                              [(display.MARGIN + self.WIDTH) * column + display.MARGIN,
-    #                               (display.MARGIN + self.HEIGHT) * row + display.MARGIN,
-    #                               self.WIDTH,
-    #                               self.HEIGHT])
-    #
-    #     # Limit to 60 frames per second
-    #     self.clock.tick(60)
-    #
-    #     # Go ahead and update the screen with what we've drawn.
-    #     pygame.display.flip()
+    def grab_screen(self):
+        return pygame.surfarray.array3d(self.screen)
+
+    def frame_step(self, input_actions):
+        """
+        :param input_actions: One-hot encoding of actions (eg. up = [1, 0, 0, 0])
+        :return: (next_state, reward)
+        """
+        self.steps += 1
+        self.screen.fill(BLACK)
+
+        if input_actions[0] == 1: # up
+            if self.y > 0:
+                self.y -= 1
+        elif input_actions[1] == 1: # down
+            if self.y < self.grid_size[1] - 1:
+                self.y += 1
+        elif input_actions[2] == 1: # left
+            if self.x > 0:
+                self.x -= 1
+        elif input_actions[3] == 1: # right
+            if self.x < self.grid_size[0] - 1:
+                self.x += 1
+        self.draw_agent()
+
+        return self.grab_screen(), 0
 
     def quit(self):
         pygame.quit()
 
-
 def main():
     """
-    Test Method - wasd to move.
+    For Testing
     """
-    dom = ShapeWorld(params)
-    # gui = display(fighter.grid_size, fighter.grid_to_pixel)
-    # gui.draw(fighter)
-    # # print("New Instance: Agent: ", fighter.agent, "Fire(s): ", fighter.fire, " Water(s): ", fighter.water)
-    # inp = None
-    # while (inp != "l"):
-    #     pass
-    #     # inp = raw_input("Input: ")
-    #     # mapping = {"a": 0, "d": 1, "w": 2, "s": 3, "z": 4, "x": 5}
-    #     # if inp in mapping:
-    #     #     fighter.execute_action(mapping[inp])
-    #     #     gui.draw(fighter.disp_arr)
-    #     # elif inp == 'l':
-    #     #     print("Quitting")
-    #     # else:
-    #     #     print("Illegal")
-    #     #     # print("Agent: " , fighter.agent, "Fire: ", fighter.fire , " Water: ", fighter.water, " Has Water: ", fighter.has_water)
-    # gui.quit()
+    test = False
+    if test:
+        domain = ShapeWorld(params)
+        res = domain.grab_screen()
 
-    # TODO: add -d flag for display mode
+        from PIL import Image
+        Image.fromarray(res).show()
 
+        up = [1, 0, 0, 0]
+        down = [0, 1, 0, 0]
+        left = [0, 0, 1, 0]
+        right = [0, 0, 0, 1]
+
+        test_seq = [up, down, down, left, right, right, down, left, up, right]
+        for action in test_seq:
+            _ = raw_input("Press to move {}".format(action))
+            res, _ = domain.frame_step(action)
+            print(domain.x, domain.y)
+            Image.fromarray(res).show()
+
+        domain.quit()
 
 if __name__ == "__main__":
     main()
